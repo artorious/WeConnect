ff9670532b8dc3382d5962f219d6721655b97ed3
Arthur Ngondo <ngondoarthur@gmail.com>
2018-03-05 23:05:47 +0300

    Add failing tests for root/home page

---
+++ app/test_views.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+from app import app
+
+import unittest
+
+class SimpleTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_index_http_method(self):
+        response = self.app.get('/')
+        self.assertIn('GET', str(response.data))
+
+    def test_index_http_response_status(self):
+        response = self.app.get('/')
+        self.assertEqual(404, response.status_code) # Route not set up
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+
+
--- app/views.py
+++ app/views.py
@@ -2,7 +2,11 @@
 """ Routes for app. Tells lask what to display on which path """
 
 from flask import request
+from app import app
 
-@app.route('/')
-def index():
-   pass
+@app.route('/', methods=['GET'])
+def index(): 
+    """ Return method made by the user to request resource """
+    pass
+    
+
